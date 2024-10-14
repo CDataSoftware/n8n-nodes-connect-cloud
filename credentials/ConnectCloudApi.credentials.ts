65a84035a894b079a95cba350776f8e800e8ed68
Blake Martin <blakem@cdata.com>
2024-10-14 11:48:04 -0400

    initial work

---
+++ credentials/ConnectCloudApi.credentials.ts
@@ -0,0 +1,33 @@
+import { ICredentialType, INodeProperties } from 'n8n-workflow';
+
+export class ConnectCloudApi implements ICredentialType {
+  displayName = 'Connect Cloud API';
+  name = 'connectCloudApi';
+  version = 1;
+  // Define globally available credentials
+  scope = ['global'];
+
+  // Define the credentials properties
+  properties: INodeProperties[] = [
+    {
+      displayName: 'Username (Email)',
+      name: 'username',
+      type: 'string',
+      default: '',
+      placeholder: 'user@cdata.com',
+      required: true,
+      description: 'The username used for authentication, formatted as an email address',
+    },
+    {
+      displayName: 'Personal Access Token (PAT)',
+      name: 'token',
+      type: 'string',
+      typeOptions: {
+        password: true, // This will hide the token for security purposes
+      },
+      default: '',
+      required: true,
+      description: 'The Personal Access Token (PAT) for Connect Cloud authentication',
+    },
+  ];
+}
